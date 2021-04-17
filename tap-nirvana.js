de00fe9a9fe230619e9c7591314335b3f61ac123
Sergio de Cristofaro <sdecri@gmail.com>
2021-04-17 19:16:53 +0200

    refactor - extracted function to print a failed assertion

--- tap-nirvana.js
+++ tap-nirvana.js
@@ -86,55 +86,8 @@ module.exports = function (spec) {
 
   // Failing assertions
   parser.on('fail', function (assertion) {
-
-    var glyph = symbols.cross;
-    var title =  glyph + pad(assertion.name);
-    var divider = _.fill(
-      new Array((title).length + 1),
-      '-'
-    ).join('');
-    output.push(pad('  ' + format.red(title) + '\n'));
-    output.push(pad('  ' + format.red(divider) + '\n'));
-
-    let skipObjectDiff = true;
-    let errorMessage  = format.magenta("operator:") + " deepEqual\n";
-
-    if (assertion.error.operator === 'deepEqual') {
-      skipObjectDiff = false;
-      try {
-        const exObj = reviveJSON(assertion.error.expected);
-        const acObj = reviveJSON(assertion.error.actual);
-        const expected = stringify(exObj);
-        const actual = stringify(acObj);
-
-        if (typeof exObj == 'object' && typeof acObj == 'object') {
-          errorMessage += format.magenta("expected: ") + expected + "\n";
-          var difference = vdiff(exObj, acObj).text;
-          errorMessage += format.magenta("diff: ") + difference + "\n";
-          const moreUsefulStack = removeUselessStackLines(assertion.error.stack);
-          errorMessage += format.magenta("source: ") + format.gray(moreUsefulStack) + "\n";   
-        } else {
-          skipObjectDiff = true;
-        }
-      } catch (err) {
-        console.log("error fired " + err);
-        skipObjectDiff = true;
-      }
-    }
-
-    if (skipObjectDiff) {
-      const expected = assertion.error.expected;
-      const actual = assertion.error.actual;
-      //errorMessage += format.magenta("expected: ") + expected + "\n";
-      const delta = vdiff(expected, actual).text;
-      errorMessage += format.magenta("diff: ") + delta + "\n";
-      const moreUsefulStack = removeUselessStackLines(assertion.error.stack);
-      errorMessage += format.magenta("source: ") + format.gray(moreUsefulStack) + "\n"; 
-    }
-
-    errorMessage = prettifyRawError(errorMessage, 3);
-    output.push(errorMessage);
-
+    output.push(formatFailedAssertion(assertion))
+   
     stream.failed = true;
   });
 
@@ -186,6 +139,7 @@ module.exports = function (spec) {
     return pretty;
   }
 
+
   // this duplicates errors that we already showd.
   // @TODO : remove
   function formatErrors (results) {
@@ -212,6 +166,62 @@ module.exports = function (spec) {
            pad('of ' + results.asserts.length + ' tests') +
            pad(format.dim('(' + prettyMs(new Date().getTime() - startTime) + ')'));
   }
+
+  function formatFailedAssertion (assertion) {
+    
+    var glyph = symbols.cross;
+    var title =  glyph + pad(assertion.name);
+    var divider = _.fill(
+      new Array((title).length + 1),
+      '-'
+    ).join('');
+
+    let out = '';
+    out += pad('  ' + format.red(title) + '\n');
+    out += pad('  ' + format.red(divider) + '\n');
+    
+
+    let skipObjectDiff = true;
+    let errorMessage  = format.magenta("operator:") + " deepEqual\n";
+
+    if (assertion.error.operator === 'deepEqual') {
+      skipObjectDiff = false;
+      try {
+        const exObj = reviveJSON(assertion.error.expected);
+        const acObj = reviveJSON(assertion.error.actual);
+        const expected = stringify(exObj);
+        const actual = stringify(acObj);
+
+        if (typeof exObj == 'object' && typeof acObj == 'object') {
+          errorMessage += format.magenta("expected: ") + expected + "\n";
+          var difference = vdiff(exObj, acObj).text;
+          errorMessage += format.magenta("diff: ") + difference + "\n";
+          const moreUsefulStack = removeUselessStackLines(assertion.error.stack);
+          errorMessage += format.magenta("source: ") + format.gray(moreUsefulStack) + "\n";
+        } else {
+          skipObjectDiff = true;
+        }
+      } catch (err) {
+        console.log("error fired " + err);
+        skipObjectDiff = true;
+      }
+    }
+
+    if (skipObjectDiff) {
+      const expected = assertion.error.expected;
+      const actual = assertion.error.actual;
+      //errorMessage += format.magenta("expected: ") + expected + "\n";
+      const delta = vdiff(expected, actual).text;
+      errorMessage += format.magenta("diff: ") + delta + "\n";
+      const moreUsefulStack = removeUselessStackLines(assertion.error.stack);
+      errorMessage += format.magenta("source: ") + format.gray(moreUsefulStack) + "\n";
+    }
+
+    errorMessage = prettifyRawError(errorMessage, 3);
+    out += (errorMessage);
+
+    return out;
+  };
 
   function formatFailedAssertions (results) {
 
